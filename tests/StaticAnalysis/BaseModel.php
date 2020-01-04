105a93cd321946806f6472c6e2e3d6c0c416402d
Andreas Möller <am@localheinz.com>
2020-01-04 13:20:28 +0100

    Enhancement: Add file-level header

--- tests/StaticAnalysis/BaseModel.php
+++ tests/StaticAnalysis/BaseModel.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * Copyright (c) 2018 Jan Gregor Emge-Triebel
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/Jan0707/phpstan-prophecy
+ */
+
 namespace JanGregor\Prophecy\Test\StaticAnalysis;
 
 class BaseModel
